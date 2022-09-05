3bc0b6951dadc36ea581462c1a29e65fc35ebc15
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-09-06 08:28:19 +0900

    bug: string배열에 참조만 했는데 쓰레기값으로 바뀜.

--- ps/queue/bfs/2206.cpp
+++ ps/queue/bfs/2206.cpp
@@ -16,35 +16,28 @@ int main(void)
 
 	int N, M;
 	int dist[1000][1000]; // N, M까지 가는데 최소거리
-	int dist2[1000][1000]; // N, M까지 가는데 최소거리
-	string board[N];
+	cin >> N >> M;
 
+	int board[1000][1000];
 	fill(&dist[0][0], &dist[999][1000], -1);
 	queue<tuple<int, int, int>> Queue;
 
-	cin >> N >> M;
+	string s_board[1000];
 	for (int idx = 0; idx < N; ++idx) 
 	{
-		cin >> board[idx];
+		cin >> s_board[idx];
 		for (int jdx = 0; jdx < M; ++jdx) 
-		{
-			if (board[idx][jdx] == '1')
-			{
-				dist2[idx][jdx] = 0;
-			}
-		}
+			board[idx][jdx] = s_board[idx][jdx] - '0';
 	}
 	Queue.push({0, 0, 0});
 	dist[0][0] = 1;
 	int flag = 0;
 	while (!Queue.empty())
 	{
-		//sleep(2);
 		auto cur = Queue.front();
-		dist2[get<0>(cur)][get<1>(cur)] = get<2>(cur);
-		cout << "cur<0>: " << get<0>(cur) << " cur<1>: " << get<1>(cur) << " cur<2>: " << get<2>(cur) << '\n';
+		cout << get<0>(cur) << ' ' << get<1>(cur) << ' ' << get<2>(cur) << '\n';
 		Queue.pop();
-		if (get<0>(cur) == N - 1 && get<1>(cur) == M - 1 && dist2[get<0>(cur)][get<1>(cur)] <= 1)
+		if (get<0>(cur) == N - 1 && get<1>(cur) == M - 1 && get<2>(cur) <= 1)
 		{
 			flag = 1;
 			cout << dist[get<0>(cur)][get<1>(cur)] << '\n';
@@ -57,18 +50,17 @@ int main(void)
 				}
 				cout << '\n';
 			}
-			cout << "dist2\n";
+			cout << "board\n";
 			for (int idx = 0; idx < N; ++idx) 
 			{
 				for (int jdx = 0; jdx < M; ++jdx) 
 				{
-					cout << dist2[idx][jdx] << ' ';
+					cout << board[idx][jdx] << ' ';
 				}
 				cout << '\n';
 			}
 			break ;
 		}
-		// 0 1 1 
 		int temp = get<2>(cur);
 		for (int idx = 0; idx < 4; ++idx) 
 		{
@@ -78,20 +70,13 @@ int main(void)
 
 			int ny = get<0>(cur) + dy[idx];
 			int nx = get<1>(cur) + dx[idx];
-			cout << "before: get<2>(cur): " << get<2>(cur) << '\n';
 			if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-			{
-				cout << "여긴가?\n";
 				continue ;
-			}
-			if (board[ny][nx] == '1')
+			if (board[ny][nx] == 1)
 				get<2>(cur) += 1;
 			if (dist[ny][nx] != -1)
-			{
-				cout << "여긴가2?\n";
 				continue ;
-			}
-			if (board[ny][nx] == '1' && get<2>(cur) > 1)
+			if (board[ny][nx] == 1 && get<2>(cur) > 1)
 			{
 				dist[ny][nx] = 0;
 				continue ;
@@ -102,4 +87,22 @@ int main(void)
 	}
 	if (!flag)
 		cout << "-1\n";
+			cout << '\n';
+			for (int idx = 0; idx < N; ++idx) 
+			{
+				for (int jdx = 0; jdx < M; ++jdx) 
+				{
+					cout << dist[idx][jdx] << ' ';
+				}
+				cout << '\n';
+			}
+			cout << "board\n";
+			for (int idx = 0; idx < N; ++idx) 
+			{
+				for (int jdx = 0; jdx < M; ++jdx) 
+				{
+					cout << board[idx][jdx] << ' ';
+				}
+				cout << '\n';
+			}
 }
