9117a599a99e1a36753165c186999ce933d7ed24
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-09-07 07:08:53 +0900

    될듯 말듯, 천천히 생각하면 할 수 있는데,,

--- ps/queue/bfs/2206.cpp
+++ ps/queue/bfs/2206.cpp
@@ -8,29 +8,29 @@
 using namespace std;
 typedef long long ll;
 
-
+int dist[1000][1000][2];
+int board[1000][1000];
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int N, M;
-	int dist[1000][1000]; // N, M까지 가는데 최소거리
 	cin >> N >> M;
 
-	int board[1000][1000];
-	fill(&dist[0][0], &dist[999][1000], -1);
+	memset(dist, -1, sizeof(dist));
 	queue<tuple<int, int, int>> Queue;
 
-	string s_board[1000];
+	string str;
 	for (int idx = 0; idx < N; ++idx) 
 	{
-		cin >> s_board[idx];
+		cin >> str;
 		for (int jdx = 0; jdx < M; ++jdx) 
-			board[idx][jdx] = s_board[idx][jdx] - '0';
+			board[idx][jdx] = str[jdx] - '0';
 	}
 	Queue.push({0, 0, 0});
-	dist[0][0] = 1;
+	dist[0][0][0] = 1;
+	dist[0][0][1] = 1;
 	int flag = 0;
 	while (!Queue.empty())
 	{
@@ -40,16 +40,11 @@ int main(void)
 		if (get<0>(cur) == N - 1 && get<1>(cur) == M - 1 && get<2>(cur) <= 1)
 		{
 			flag = 1;
-			cout << dist[get<0>(cur)][get<1>(cur)] << '\n';
+			int temp = dist[get<0>(cur)][get<1>(cur)][1];
+			if (dist[get<0>(cur)][get<1>(cur)][0] >= temp)
+				temp = dist[get<0>(cur)][get<1>(cur)][0];
+			cout << temp << '\n';
 			cout << '\n';
-			for (int idx = 0; idx < N; ++idx) 
-			{
-				for (int jdx = 0; jdx < M; ++jdx) 
-				{
-					cout << dist[idx][jdx] << ' ';
-				}
-				cout << '\n';
-			}
 			cout << "board\n";
 			for (int idx = 0; idx < N; ++idx) 
 			{
@@ -71,38 +66,56 @@ int main(void)
 			int ny = get<0>(cur) + dy[idx];
 			int nx = get<1>(cur) + dx[idx];
 			if (ny < 0 || ny >= N || nx < 0 || nx >= M)
-				continue ;
-			if (board[ny][nx] == 1)
-				get<2>(cur) += 1;
-			if (dist[ny][nx] != -1)
-				continue ;
-			if (board[ny][nx] == 1 && get<2>(cur) > 1)
 			{
-				dist[ny][nx] = 0;
+				cout << "범위 초과\n";
 				continue ;
 			}
-			dist[ny][nx] = dist[get<0>(cur)][get<1>(cur)] + 1;
+			if (board[ny][nx] == 1)
+				get<2>(cur) += 1;
+			if (board[ny][nx] == 1 && get<2>(cur) > 1)
+			{
+				cout << "get<2>가 2이상이다.\n";
+				dist[ny][nx][get<2>(cur)] = 0;
+				continue ;
+			}
+			if (dist[ny][nx][get<2>(cur)] != -1)
+			{
+				cout << "dist가 -1이 아니다\n";
+				continue ;
+			}
+			cout << "dist값 올려줄꺼임\n";
+			dist[ny][nx][get<2>(cur)] = dist[get<0>(cur)][get<1>(cur)][get<2>(cur)] + 1;
 			Queue.push({ny, nx, get<2>(cur)});
 		}
 	}
 	if (!flag)
 		cout << "-1\n";
 			cout << '\n';
-			for (int idx = 0; idx < N; ++idx) 
-			{
-				for (int jdx = 0; jdx < M; ++jdx) 
-				{
-					cout << dist[idx][jdx] << ' ';
-				}
-				cout << '\n';
-			}
-			cout << "board\n";
-			for (int idx = 0; idx < N; ++idx) 
-			{
-				for (int jdx = 0; jdx < M; ++jdx) 
-				{
-					cout << board[idx][jdx] << ' ';
-				}
-				cout << '\n';
-			}
+	cout << "dist0\n";
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		for (int jdx = 0; jdx < M; ++jdx) 
+		{
+			cout << dist[idx][jdx][0] << ' ';
+		}
+		cout << '\n';
+	}
+	cout << "dist1\n";
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		for (int jdx = 0; jdx < M; ++jdx) 
+		{
+			cout << dist[idx][jdx][1] << ' ';
+		}
+		cout << '\n';
+	}
+	cout << "board\n";
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		for (int jdx = 0; jdx < M; ++jdx) 
+		{
+			cout << board[idx][jdx] << ' ';
+		}
+		cout << '\n';
+	}
 }
