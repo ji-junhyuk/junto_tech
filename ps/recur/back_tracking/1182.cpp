ff3869f090e3b77c6750355bb687045b83af1d7b
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-09-30 08:15:57 +0900

    feat: add next_permutation solve

--- ps/recur/back_tracking/1182.cpp
+++ ps/recur/back_tracking/1182.cpp
@@ -7,36 +7,64 @@
 using namespace std;
 typedef long long ll;
 
-int visited[21];
-int sequence[21];
-
-void dfs(int depth, int &cnt, int sum, int N, int S)
-{
-	if (depth == N)
-	{
-		if (sum == S)
-			++cnt;
-		return ;
-	}
-	dfs(depth + 1, cnt, sum, N, S);
-	dfs(depth + 1, cnt, sum + sequence[depth], N, S);
-}
-
+int sequence[20];
+int comb[20];
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, S;
-	int cnt;
+	int N, S, sum, cnt;
+
+	sum = 0;
+	cnt = 0;
 	cin >> N >> S;
 	for (int idx = 0; idx < N; ++idx) 
-	{
 		cin >> sequence[idx];
+	for (int idx = 0; idx < 20; ++idx) 
+		comb[idx] = 1;
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		comb[idx] = 0;
+		do {
+			sum = 0;
+			for (int jdx = 0; jdx < N; ++jdx) 
+			{
+				if (!comb[jdx])
+					sum += sequence[jdx];
+			}
+			if (sum == S)
+				++cnt;
+		} while (next_permutation(comb, comb + N));
 	}
-	cnt = 0;
-	dfs(0, cnt, 0, N, S);
-	if (S == 0)
-		--cnt;
 	cout << cnt;
 }
+//void	recur(int depth, int sum, int &cnt, int N, int S)
+//{
+//	if (depth == N)
+//	{
+//		if (sum == S)
+//			cnt += 1;
+//		return ;
+//	}
+//	recur(depth + 1, sum + sequence[depth], cnt, N, S);
+//	recur(depth + 1, sum, cnt, N, S);
+//}
+
+//int main(void)
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int N, S;
+//	int cnt;
+//
+//	cin >> N >> S;
+//	for (int idx = 0; idx < N; ++idx) 
+//		cin >> sequence[idx];
+//	cnt = 0;
+//	recur(0, 0, cnt, N, S);
+//	if (S == 0)
+//		--cnt;
+//	cout << cnt << '\n';
+//}
