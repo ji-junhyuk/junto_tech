3d4fba2c4c28b89ef0ef34826a1700fbdf2f4d79
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-10-07 07:59:47 +0900

    feat: solve next_permutation(s3)

--- ps/recur/back_tracking/15651.cpp
+++ ps/recur/back_tracking/15651.cpp
@@ -7,34 +7,60 @@
 using namespace std;
 typedef long long ll;
 
-int visited[9];
-int sequence[8];
-
-void dfs(int depth, int N, int M)
-{
-	if (depth == M)
-	{
-		for (int idx = 0; idx < M; ++idx) 
-			cout << sequence[idx] << ' ';
-		cout << '\n';
-		return ;
-	}
-	for (int idx = 0; idx < N; ++idx) 
-	{
-		visited[idx] = 1;
-		sequence[depth] = idx + 1;
-		dfs(depth + 1, N, M);
-		visited[idx] = 0;
-	}
-}
-
 int main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int N, M;
+	int sequence[8];
 
 	cin >> N >> M;
-	dfs(0, N, M);
+	vector<int> V;
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		V.push_back(idx + 1);
+		for (int jdx = 1; jdx < M; ++jdx) 
+		{
+			if (idx < N)
+				V.push_back(idx + 1);
+		}
+	}
+	do {
+		for (int idx = 0; idx < M; ++idx) 
+			cout << V[idx] << ' ';
+		cout << '\n';
+		reverse(V.begin() + M, V.end());
+	} while (next_permutation(V.begin(), V.end()));
 }
+//int visited[9];
+//int sequence[8];
+//
+//void dfs(int depth, int N, int M)
+//{
+//	if (depth == M)
+//	{
+//		for (int idx = 0; idx < M; ++idx) 
+//			cout << sequence[idx] << ' ';
+//		cout << '\n';
+//		return ;
+//	}
+//	for (int idx = 0; idx < N; ++idx) 
+//	{
+//		visited[idx] = 1;
+//		sequence[depth] = idx + 1;
+//		dfs(depth + 1, N, M);
+//		visited[idx] = 0;
+//	}
+//}
+//
+//int main(void)
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int N, M;
+//
+//	cin >> N >> M;
+//	dfs(0, N, M);
+//}
