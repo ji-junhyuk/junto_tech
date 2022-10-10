ea43fe805453c1b933da84ee6c159ad1b8ad57f0
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-10-11 07:10:35 +0900

    feat: sequence(s3)

---
+++ ps/recur/back_tracking/15656.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#include <tuple>
+using namespace std;
+typedef long long ll;
+
+int N, M;
+int arr[8];
+int sequence[8];
+
+void	dfs(int depth)
+{
+	if (depth == M)
+	{
+		for (int idx = 0; idx < M; ++idx) 
+			cout << sequence[idx] << ' ';
+		cout << '\n';
+		return ;
+	}
+	for (int idx = 0; idx < N; ++idx) 
+	{
+		sequence[depth] = arr[idx];
+		dfs(depth + 1);
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	for (int idx = 0; idx < N; ++idx) 
+		cin >> arr[idx];
+	sort(arr, arr + N);
+	dfs(0);
+}
