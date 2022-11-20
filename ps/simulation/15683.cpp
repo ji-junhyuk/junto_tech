bba74acec8fd8be1d721e1ffcf509f897f40b750
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-20 10:13:31 +0900

    feat: resolve 15683(g4)

--- ps/simulation/15683.cpp
+++ ps/simulation/15683.cpp
@@ -4,114 +4,161 @@
 #include <algorithm>
 #include <cstring>
 #include <tuple>
-using namespace std;
-typedef long long ll;
+#include <utility>
+#include <vector>
 
-int N, M;
-int temp_area[8][8];
-
-bool is_out_of_bound(int y, int x)
+class Office
 {
-	if (y < 0 || y >= N || x < 0 || x >= M)
-		return true;
-	return false;
-}
+	public:
+		int row, col;
+		int office[8][8];
+		int temp_office[8][8];
+		int blind_spot_count = 0;
+		std::vector<std::pair<int, int>> cctv;
+	
+		Office()
+		{
+			input();
+		}
 
-void	update_cctv_path(int y, int x, int dir)
-{
-	dir %= 4;
-	int dy[] = {1, 0, -1, 0};
-	int dx[] = {0, 1, 0, -1};
-	while (1)
-	{
-		y += dy[dir];
-		x += dx[dir];
-		if (is_out_of_bound(y, x) || temp_area[y][x] == 6)
-			return ;
-		if (temp_area[y][x] != 0)
-			continue ;
-		temp_area[y][x] = -1;
-	}
-}
+		void	input()
+		{
+			std::cin >> this->row >> this->col;
+			for (int idx = 0; idx < row; ++idx) 
+			{
+				for (int jdx = 0; jdx < col; ++jdx) 
+				{
+					std::cin >> this->office[idx][jdx];
+				}
+			}
+		}
+
+		void	count_blind_spot_and_cctv_count()
+		{
+			for (int idx = 0; idx < row; ++idx) 
+			{
+				for (int jdx = 0; jdx < col; ++jdx) 
+				{
+					if (office[idx][jdx] == 0)
+						++(this->blind_spot_count);
+					if (1 <= office[idx][jdx] && office[idx][jdx] <= 5)
+						cctv.push_back({idx, jdx});
+				}
+			}
+		}
+
+		bool is_out_of_bound(int y, int x)
+		{
+			if (y < 0 || y >= this->row || x < 0 || x >= this->col)
+				return true;
+			return false;
+		}
+
+		void update_cctv_path(int y, int x, int dir)
+		{
+			int dy[] = {1, 0, -1, 0};
+			int dx[] = {0, 1, 0, -1};
+
+			dir %= 4;
+			while (1)
+			{
+				y += dy[dir];
+				x += dx[dir];
+				if (is_out_of_bound(y, x) || office[y][x] == 6)
+					return ;
+				if (office[y][x] != 0)
+					continue ;
+				this->temp_office[y][x] = -1;
+			}
+		}
+
+		void	find_minimum_blind_spot_count()
+		{
+			for (int idx = 0; idx < (1 << (2 * this->cctv.size())); ++idx) 
+			{
+				for (int jdx = 0; jdx < this->row; ++jdx) 
+				{
+					for (int kdx = 0; kdx < this->col; ++kdx) 
+					{
+						temp_office[jdx][kdx] = office[jdx][kdx];
+					}
+				}
+				int brute = idx;
+				for (int tmp = 0; tmp < this->cctv.size(); ++tmp) 
+				{
+					int dir = brute % 4;
+					brute /= 4;
+					int y = cctv[tmp].first;
+					int x = cctv[tmp].second;
+					if (office[y][x] == 1)
+						update_cctv_path(y, x, dir);
+					else if (office[y][x] == 2)
+					{
+						update_cctv_path(y, x, dir);
+						update_cctv_path(y, x, dir + 2);
+					}
+					else if (office[y][x] == 3)
+					{
+						update_cctv_path(y, x, dir);
+						update_cctv_path(y, x, dir + 1);
+					}
+					else if (office[y][x] == 4)
+					{
+						update_cctv_path(y, x, dir);
+						update_cctv_path(y, x, dir + 1);
+						update_cctv_path(y, x, dir + 2);
+					}
+					else
+					{
+						update_cctv_path(y, x, dir);
+						update_cctv_path(y, x, dir + 1);
+						update_cctv_path(y, x, dir + 2);
+						update_cctv_path(y, x, dir + 3);
+					}
+				}
+				int blind_spot_count = 0;
+				for (int idx = 0; idx < this->row; ++idx) 
+				{
+					for (int jdx = 0; jdx < this->col; ++jdx) 
+					{
+						blind_spot_count += (temp_office[idx][jdx] == 0);
+					}
+				}
+				this->blind_spot_count = std::min(this->blind_spot_count, blind_spot_count);
+			}
+		}
+
+		void	print_office()
+		{
+			std::cout << "row: " << row << " col: " << col << " blind_spot_count: " << blind_spot_count << " cctv_count: " << cctv.size() << '\n';
+			for (int idx = 0; idx < row; ++idx) 
+			{
+				for (int jdx = 0; jdx < col; ++jdx) 
+				{
+					std::cout << office[idx][jdx] << ' ';
+				}
+				std::cout << '\n';
+			}
+			std::cout << "temp" << '\n';
+			for (int idx = 0; idx < row; ++idx) 
+			{
+				for (int jdx = 0; jdx < col; ++jdx) 
+				{
+					std::cout << temp_office[idx][jdx] << ' ';
+				}
+				std::cout << '\n';
+			}
+		}
+};
+
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
 
-	int area[8][8];
-	int empty_area_count = 0;
-	vector<pair<int, int>> cctv;
-	
-	cin >> N >> M;
-	for (int idx = 0; idx < N; ++idx) 
-	{
-		for (int jdx = 0; jdx < M; ++jdx) 
-		{
-			cin >> area[idx][jdx];
-			if (area[idx][jdx] == 0)
-			{
-				++empty_area_count;
-			}
-			if (area[idx][jdx] >= 1 && area[idx][jdx] <= 5)
-			{
-				cctv.push_back({idx, jdx});
-			}
-		}
-	}
-	for (int tmp = 0; tmp < (1 << (2*cctv.size())); ++tmp) 
-	{
-		for (int idx = 0; idx < N; ++idx) 
-		{
-			for (int jdx = 0; jdx < M; ++jdx) 
-			{
-				temp_area[idx][jdx] = area[idx][jdx];
-			}
-		}
-		int brute = tmp;
-		for (int idx = 0; idx < cctv.size(); ++idx) 
-		{
-			int dir = brute % 4;
-			brute /= 4;
-			int y, x;
-			tie(y, x) = cctv[idx];
-			if (temp_area[y][x] == 1)
-			{
-				update_cctv_path(y, x, dir);
-			}
-			else if (temp_area[y][x] == 2)
-			{
-				update_cctv_path(y, x, dir);
-				update_cctv_path(y, x, dir + 2);
-			}
-			else if (temp_area[y][x] == 3)
-			{
-				update_cctv_path(y, x, dir);
-				update_cctv_path(y, x, dir + 1);
-			}
-			else if (temp_area[y][x] == 4)
-			{
-				update_cctv_path(y, x, dir);
-				update_cctv_path(y, x, dir + 1);
-				update_cctv_path(y, x, dir + 2);
-			}
-			else
-			{
-				update_cctv_path(y, x, dir);
-				update_cctv_path(y, x, dir + 1);
-				update_cctv_path(y, x, dir + 2);
-				update_cctv_path(y, x, dir + 3);
-			}
-		}
-		int blind_spot = 0;
-		for (int idx = 0; idx < N; ++idx) 
-		{
-			for (int jdx = 0; jdx < M; ++jdx) 
-			{
-				if (temp_area[idx][jdx] == 0)
-					++blind_spot;
-			}
-		}
-		empty_area_count = min(empty_area_count, blind_spot);
-	}
-	cout << empty_area_count << '\n';
+	Office office;
+
+	office.count_blind_spot_and_cctv_count();
+	office.find_minimum_blind_spot_count();
+	std::cout << office.blind_spot_count;
 }
